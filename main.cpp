0acb9868e021089ae04e13f1c3d59456200fd0dc
aibaceta2000 <aibaceta2022@outlook.com>
2023-04-23 11:59:58 -0400

    unifor cost search completed

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 #include <map>
 #include <random>
 #include <algorithm>
+#include <tuple>
+#include <queue>
 
 using namespace std;
 
@@ -18,6 +20,16 @@ struct graph_node{
 bool custom_operator(pair<char, int> x, pair<char, int> y)
 {
     return x.second < y.second; //return the one with smaller first element
+}
+
+bool custom_operator_uniform_cost_search(std::tuple<char, int, std::vector<char>> x, std::tuple<char, int, std::vector<char>> y)
+{
+    return get<1>(x) > get<1>(y); //return the one with smaller first element
+}
+
+bool custom_operator_tuple(tuple<int, char> x, tuple<int, char> y)
+{
+    return get<0>(x) > get<0>(y); //return the one with smaller first element
 }
 
 int rand_generator(int a, int b) {
@@ -54,12 +66,63 @@ void uniform_cost_search(char start, char goal, std::map<char, graph_node>  grap
     char current_node = start;
     vector<char> history;
     int total = 0;
+    std::priority_queue<std::tuple<char, int, std::vector<char>>, std::vector<std::tuple<char, int, std::vector<char>>>, std::function<bool(std::tuple<char, int, std::vector<char>>, std::tuple<char, int, std::vector<char>>)>> pq(custom_operator_uniform_cost_search);
+    history.push_back(current_node);
+    pq.push(std::make_tuple(graph.at(current_node).identifier, 0, history));
+    std::tuple<char, int, std::vector<char>> final;
+    while (get<0>(pq.top()) != goal)
+    {
+        for (auto &&nd : graph.at(get<0>(pq.top())).edges)
+        {
+            std::vector<char> temp = get<2>(pq.top());
+            temp.push_back(nd.first);
+            pq.push(std::make_tuple(nd.first, get<1>(pq.top()) + nd.second, temp));
+        }
+        std::cout << "current node: "<<get<0>(pq.top()) << " current distance: "<< get<1>(pq.top())<<" historial: ";
+        for (auto &&temp : get<2>(pq.top()))
+        {
+            std::cout<<temp<<", ";
+        }
+        std::cout<<std::endl;
+        
+        pq.pop();
+        if (get<0>(pq.top()) == goal)
+        {
+            std::cout << "current node: "<<get<0>(pq.top()) << " current distance: "<< get<1>(pq.top())<<" historial: ";
+            for (auto &&temp : get<2>(pq.top()))
+            {
+                std::cout<<temp<<", ";
+            }
+            std::cout<<std::endl;            
+        }
+        if (get<0>(pq.top()) == goal)
+        {
+            final = pq.top();
+        }            
+    }
+    std::cout << "final node: "<<get<0>(pq.top()) << " final distance: "<< get<1>(pq.top())<<" historial: ";
+    for (auto &&temp : get<2>(pq.top()))
+        {
+            std::cout<<temp<<", ";
+        }
+    std::cout<<std::endl;         
+}
+
+void greedy(char start, char goal, std::map<char, graph_node>  graph){
+    char current_node = start;
+    vector<char> history;
+    int total = 0;
     while (current_node != goal)
     {
        history.push_back(current_node);
-       std::sort(graph.at(current_node).edges.begin(),graph.at(current_node).edges.end(),custom_operator);
-       total += graph.at(current_node).edges.at(0).second;
-       current_node = graph.at(current_node).edges.at(0).first;
+       std::vector<std::pair<char, int>> heuristic_compare;
+       for (auto &&node : graph.at(current_node).edges)
+       {
+            heuristic_compare.push_back(std::make_pair(node.first, graph.at(node.first).heuristic_value));
+       }
+       std::sort(heuristic_compare.begin(),heuristic_compare.end(),custom_operator);
+       total += graph.at(current_node).heuristic_value;
+       current_node = heuristic_compare.at(0).first;
     }
     history.push_back(goal);
     for (auto &&node : history)
@@ -137,5 +200,27 @@ int main(int argc, char const *argv[])
 
     //dfs(start, goal, graph);
     uniform_cost_search(start, goal, graph);
+    //greedy(start, goal, graph);
+    /*std::vector<std::tuple<int,char>> nodos;
+    nodos.push_back(std::make_tuple(5,'a'));
+    nodos.push_back(std::make_tuple(6,'a'));
+    nodos.push_back(std::make_tuple(7,'a'));
+    nodos.push_back(std::make_tuple(8,'a'));
+
+    std::priority_queue<std::tuple<int,char>, std::vector<std::tuple<int,char>>, std::function<bool(std::tuple<int,char>, std::tuple<int,char>)>> pq(custom_operator_tuple);
+    pq.push(std::make_tuple(4,'a'));
+    pq.push(std::make_tuple(8,'a'));
+    pq.push(std::make_tuple(5,'a'));
+    pq.push(std::make_tuple(1,'a'));
+    pq.push(std::make_tuple(9,'a'));
+    std::cout<<pq.size()<<std::endl;
+    for (size_t i = 0; i < 5; i++)
+    {
+        std::tuple<int,char> temp = pq.top();
+        pq.pop();
+        std::cout<<get<0>(temp) << std::endl;
+    } */
+    
+    
     return 0;
 }
